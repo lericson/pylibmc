d09b36eb6aec1f7ffa20113495c24989f46709e5
Ludvig Ericson <ludvig@lericson.se>
2011-07-25 10:35:30 +0200

    Add unit tests for pooling

---
+++ tests/test_pooling.py
@@ -0,0 +1,23 @@
+import Queue
+import pylibmc
+from nose.tools import eq_, ok_
+from tests import PylibmcTestCase
+
+class PoolTestCase(PylibmcTestCase):
+    pass
+
+class ClientPoolTests(PoolTestCase):
+    def test_simple(self):
+        p = pylibmc.ClientPool(self.mc, 2)
+        with p.reserve() as smc:
+            ok_(smc)
+            ok_(smc.set("a", 1))
+            eq_(smc["a"], 1)
+
+    def test_exhaust(self):
+        p = pylibmc.ClientPool(self.mc, 2)
+        with p.reserve() as smc1:
+            with p.reserve() as smc2:
+                self.assertRaises(Queue.Empty, p.reserve().__enter__)
+
+# TODO Thread-mapped pool tests
