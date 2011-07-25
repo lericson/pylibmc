a86234a6281cd154e7034c711c052c538f9c5415
Ludvig Ericson <ludvig@lericson.se>
2011-07-25 10:25:25 +0200

    Improve testing foundation

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,20 @@ import os
 import socket
 import unittest
 import _pylibmc, pylibmc
+from textwrap import dedent
+
+class NotAliveError(Exception):
+    template = dedent("""
+    test memcached %s:%s not alive
+
+    consider setting MEMCACHED_HOST and/or MEMCACHED_PORT
+    to something more appropriate.""").lstrip()
+
+    def __init__(self, svr):
+        (self.server_type, self.address, self.port) = svr
+
+    def __str__(self):
+        return self.template % (self.address, self.port)
 
 def get_version(addr):
     (type_, host, port) = addr
@@ -30,27 +44,43 @@ def is_alive(addr):
         version = None
     return (bool(version), version)
 
-def make_test_client(cls=pylibmc.Client, env=None, **kwds):
-    """Make a test client.
+def make_test_client(cls=pylibmc.Client, env=None, server_type="tcp",
+                     host="127.0.0.1", port=11211, **kwds):
+    """Make a test client. `env` overrides keyword arguments.
 
     >>> make_test_client() is not None
     True
     """
+    server_types = {"tcp": _pylibmc.server_type_tcp,
+                    "udp": _pylibmc.server_type_udp,
+                    "unix": _pylibmc.server_type_unix}
+
     if env is None:
         env = os.environ
 
-    test_server = (
-        _pylibmc.server_type_tcp,
-        str(env.get("MEMCACHED_HOST", "127.0.0.1")),
-        int(env.get("MEMCACHED_PORT", "11211")))
-    
+    styp = env.get("MEMCACHED_TYPE", server_type)
+    host = env.get("MEMCACHED_HOST", host)
+    port = env.get("MEMCACHED_PORT", port)
+
+    test_server = (server_types[styp], str(host), int(port))
     alive, version = is_alive(test_server)
-    assert alive, "test TCP memcached %s not alive" % (test_server[1:],)
-    return cls([test_server], **kwds)
+
+    if alive:
+        return cls([test_server], **kwds)
+    else:
+        raise NotAliveError(test_server)
 
 class PylibmcTestCase(unittest.TestCase):
+    memcached_client = pylibmc.Client
+    memcached_server_type = "tcp"
+    memcached_host = "127.0.0.1"
+    memcached_port = "11211"
+
     def setUp(self):
-        self.mc = make_test_client()
+        self.mc = make_test_client(cls=self.memcached_client,
+                                   server_type=self.memcached_server_type,
+                                   host=self.memcached_host,
+                                   port=self.memcached_port)
 
     def tearDown(self):
         self.mc.disconnect_all()
