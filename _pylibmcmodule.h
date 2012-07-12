40c7bbe0a7aa73967336533f35b3be85ece1c9b5
Kelly Wong <kelly@bluejeans.com>
2012-07-12 16:53:08 -0700

    Add touch support

--- _pylibmcmodule.h
+++ _pylibmcmodule.h
@@ -274,6 +274,7 @@ static PyObject *PylibMC_Client_get_stats(PylibMC_Client *, PyObject *);
 static PyObject *PylibMC_Client_flush_all(PylibMC_Client *, PyObject *, PyObject *);
 static PyObject *PylibMC_Client_disconnect_all(PylibMC_Client *);
 static PyObject *PylibMC_Client_clone(PylibMC_Client *);
+static PyObject *PylibMC_Client_touch(PylibMC_Client *, PyObject *);
 static PyObject *PylibMC_ErrFromMemcachedWithKey(PylibMC_Client *, const char *,
         memcached_return, const char *, Py_ssize_t);
 static PyObject *PylibMC_ErrFromMemcached(PylibMC_Client *, const char *,
@@ -349,6 +350,8 @@ static PyMethodDef PylibMC_ClientType_methods[] = {
     {"clone", (PyCFunction)PylibMC_Client_clone, METH_NOARGS,
         "Clone this client entirely such that it is safe to access from "
         "another thread. This creates a new connection."},
+    {"touch", (PyCFunction)PylibMC_Client_touch, METH_VARARGS,
+        "Change the TTL of a key."},
     {NULL, NULL, 0, NULL}
 };
 /* }}} */
