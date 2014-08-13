888e272c24087ee78fb2dcf6504f7cf7a7b0fa73
Ludvig Ericson <ludvig@lericson.se>
2014-08-13 18:04:55 +0200

    Improved benchmarker

--- bin/runbench.py
+++ bin/runbench.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
+# encoding: utf-8
+
 """Run benchmarks with build/lib.* in sys.path"""
 
 import sys
 import time
 import logging
+from functools import wraps
+from collections import namedtuple
 from contextlib import contextmanager
 
+
 logger = logging.getLogger('pylibmc.bench')
+
+Benchmark = namedtuple('Benchmark', 'name f args kwargs')
+Participant = namedtuple('Participant', 'name factory')
+
 
 def build_lib_dirname():
     from distutils.dist import Distribution
@@ -14,6 +23,7 @@ def build_lib_dirname():
     build_cmd = build(Distribution({"ext_modules": True}))
     build_cmd.finalize_options()
     return build_cmd.build_lib
+
 
 class Stopwatch(object):
     "A stopwatch that never stops"
@@ -24,6 +34,9 @@ class Stopwatch(object):
 
     def avg(self):
         return sum(self.laps) / len(self.laps)
+
+    def error(self):
+        return (max(self.laps) - min(self.laps)) / 2.0
 
     def total(self):
         return time.time() - self.t0
@@ -37,26 +50,31 @@ class Stopwatch(object):
             te = time.time()
             self.laps.append(te - t0)
 
+
 def benchmark_method(f):
     "decorator to turn f into a factory of benchmarks"
-    def inner(name, *args, **kwds):
-        return (name, (f, args, kwds))
+    @wraps(f)
+    def inner(name, *args, **kwargs):
+        return Benchmark(name, f, args, kwargs)
     return inner
 
-@benchmark_method
-def bench_get_set(ctx, key, data):
-    if not ctx.mc.set(key, data, min_compress_len=4001):
-        ctx.logger.warn('set(%r, ...) fail', key)
-    if ctx.mc.get(key) != data:
-        ctx.logger.warn('get(%r) fail', key)
 
 @benchmark_method
-def bench_get_set_multi(ctx, keys, datagen='data%s'.__mod__):
-    fails = ctx.mc.set_multi(dict((k, datagen(k)) for k in keys))
+def bench_get_set(mc, key, data):
+    if not mc.set(key, data, min_compress_len=4001):
+        logger.warn('set(%r, ...) fail', key)
+    if mc.get(key) != data:
+        logger.warn('get(%r) fail', key)
+
+
+@benchmark_method
+def bench_get_set_multi(mc, keys, datagen='data%s'.__mod__):
+    fails = mc.set_multi(dict((k, datagen(k)) for k in keys))
     if fails:
-        ctx.logger.warn('set_multi(%r) fail', fails)
-    if len(ctx.mc.get_multi(keys).keys()) != len(keys):
-        ctx.logger.warn('get_multi() incomplete')
+        logger.warn('set_multi(%r) fail', fails)
+    if len(mc.get_multi(keys).keys()) != len(keys):
+        logger.warn('get_multi() incomplete')
+
 
 complex_data_type = ([], {}, __import__('fractions').Fraction(3, 4))
 
@@ -68,40 +86,76 @@ benchmarks = [
     bench_get_set('complex i/o', 'abc', complex_data_type),
 ]
 
-def bench(mc):
-    behavior_sets = [
-        {'name':        'default'},
+participants = [
 
-        {'name':        'fast',
-         'tcp_nodelay': True,
-         'verify_keys': False,
-         'hash':        'crc',
-         'distribution': 'consistent',
-         'no_block':    True}
-    ]
+    Participant(name='pylibmc',
+               factory=lambda: __import__('pylibmc.test').test.make_test_client()),
 
-    for behaviors in behavior_sets:
-        logger.info('using behavior set %s', behaviors.pop('name'))
-        ctx = type('Context', (object,), {
-            'mc': mc(behaviors=behaviors),
-            'logger': logger,
-            'bench_time': 10.0
-        })
-        for (name, (f, a, k)) in benchmarks:
-            logger.info('benchmarking %s', name)
+    Participant(name='pylibmc nonblock',
+               factory=lambda: __import__('pylibmc.test').test.make_test_client(
+        behaviors={'tcp_nodelay':  True,
+                   'verify_keys':  False,
+                   'hash':         'crc',
+                   'no_block':     True})),
+
+    Participant(name='pylibmc binary',
+               factory=lambda: __import__('pylibmc.test').test.make_test_client(
+        binary=True,
+        behaviors={'tcp_nodelay': True,
+                   'hash':        'crc'})),
+
+    Participant(name='python-memcache',
+               factory=lambda: __import__('memcache').Client(['127.0.0.1:11211'])),
+
+]
+
+
+def bench(participants=participants, benchmarks=benchmarks, bench_time=10.0):
+    """Do you even lift?"""
+
+    mcs = [p.factory() for p in participants]
+
+    data = []
+
+    # Have each lifter do one benchmark each
+    for benchmark_name, f, args, kwargs in benchmarks:
+        logger.info('Benchmark %s', benchmark_name)
+
+        results = []
+        errors = []
+        data.append((results, errors))
+
+        for participant, mc in zip(participants, mcs):
             sw = Stopwatch()
-            while sw.total() < ctx.bench_time:
+
+            while sw.total() < bench_time:
                 with sw.timing():
-                    f(ctx, *a, **k)
-            logger.info('%.2f benches per second average', 1.0 / sw.avg())
+                    f(mc, *args, **kwargs)
+
+            results.append(1.0 / sw.avg())
+            errors.append(1.0 / sw.error())
+
+            logger.info(u'%s: %.3g±%.3g benches/sec', participant.name,
+                        1.0 / sw.avg(), 1.0 / sw.error())
+
+    print 'labels =', [p.name for p in participants]
+    print 'benchmarks =', [b.name for b in benchmarks]
+    print 'data =', data
+
 
 def main(args=sys.argv[1:]):
+    sys.path.insert(0, build_lib_dirname())
     from pylibmc import build_info
-    from pylibmc.test import make_test_client
-    logger.info('benching %s', build_info())
-    bench(make_test_client)
+    logger.info('Loaded %s', build_info())
+
+    ps = [p for p in participants if p.name in args] if args else participants
+
+    logger.info('Benchmarking %d participants in %d benchmarks',
+                len(ps), len(benchmarks))
+
+    bench(participants=ps)
+
 
 if __name__ == "__main__":
-    sys.path.insert(0, build_lib_dirname())
     logging.basicConfig(level=logging.DEBUG)
     main()
